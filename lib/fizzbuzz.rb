dff358ac27695653102bc73699d0564c81e56f76
Natalie Akam <natalie.akam@gmail.com>
2015-06-09 16:08:17 +0100

    Updated hash to include %15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,7 @@
 def fizzbuzz(number)
   h = { 'fizz' => (number % 3 == 0) && (number % 5 != 0),
-  		'buzz' => (number % 5 == 0) && (number % 3 != 0)
+  		'buzz' => (number % 5 == 0) && (number % 3 != 0),
+  		'fizzbuzz' => (number % 15 == 0) 
   }
 
   h.key(true)
